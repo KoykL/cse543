bd836935bd89ef2852ba7ada21b252b994cd21e0
koykl <koykl@users.noreply.github.com>
2017-12-08 17:43:17 -0600

    f

--- doudizhu.py
+++ doudizhu.py
@@ -4,7 +4,7 @@ import os
 from itertools import count
 from multiprocessing import set_start_method
 
-from core.agent import MctsAgent, HumanAgent
+from core.agent import MctsAgent, HumanAgent, DQLAgent
 from core.platform import Platform
 from learning.trainer import DQLTrainer
 
@@ -18,6 +18,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # parser.add_argument("-a", "--agent", default=0)
     parser.add_argument("-t", "--train", default=False, action='store_true')
+    parser.add_argument("--human_player", default=None, action="store", type=int)
     args = parser.parse_args()
     logger = logging.getLogger(None)
     if args.train:
@@ -32,23 +33,35 @@ if __name__ == "__main__":
             trainer.run_iter()
     else:
         # initialize
-        agents = [MctsAgent(i) for i in range(1, 3)]
+        ai_nums = list(range(0, 3))
+        if args.human_player is not None:
+            ai_nums.remove(args.human_player)
+        
+        agents = [DQLAgent(i, "data/model.pth", turns=15) for i in ai_nums]
+        
         for agent in agents:
             agent.start()
-        agents.insert(0, HumanAgent(0))
+        if args.human_player is not None:
+            agents.insert(args.human_player, HumanAgent(args.human_player))
         # agents.insert(2, RandomAgent())
         platform = Platform(agents)
 
         # start game
         platform.deal()
-        for i, a_s in enumerate(platform.game_state.agent_states):
-            print("agent {} has card: {}".format(i, a_s.get_cards_str()))
-
+        if args.human_player is None:
+            for i, a_s in enumerate(platform.game_state.agent_states):
+                print("agent {} has card: {}".format(i, a_s.get_cards_str()))
+            else:
+                print("you have card: {}".format(platform.game_state.agent_states[args.human_player]))
         while not platform.game_state.isTerminal():
             agent_playing = platform.game_state.whos_turn
             action = platform.turn()
             print("agent {} played: {}".format(agent_playing, action))
-            for i, a_s in enumerate(platform.game_state.agent_states):
-                print("agent {} has card: {}".format(i, a_s.get_cards_str()))
+            if args.human_player is None:
+                for i, a_s in enumerate(platform.game_state.agent_states):
+                    print("agent {} has card: {}".format(i, a_s.get_cards_str()))
+            else:
+                print("you have card: {}".format(platform.game_state.agent_states[args.human_player]))
+
         for agent in agents:
             agent.terminate()
